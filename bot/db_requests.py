202715b8b56463990269a5a5f3e205aed3833126
MarikSmerch <arambo202@gmail.com>
2025-08-04 02:04:04 +0300

    update handlers 3.0

--- bot/db_requests.py
+++ bot/db_requests.py
@@ -3,15 +3,13 @@ from db.models import User
 from datetime import datetime
 
 
-def add_user(username: str, avatar_url: str | None = None):
+def add_user(username, avatar_url):
     db = SessionLocal()
-    existing = db.query(User).filter_by(username=username).first()
-    if not existing:
-        user = User(
-            username=username,
-            avatar_url=avatar_url,
-            created_at=datetime.utcnow()
-        )
+    user = db.query(User).filter_by(username=username).first()
+    if not user:
+        user = User(username=username, avatar_url=avatar_url, created_at=datetime.utcnow())
         db.add(user)
-        db.commit()
+    else:
+        user.avatar_url = avatar_url
+    db.commit()
     db.close()
