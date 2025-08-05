375210b9dfbc141c4c4f5dfe0e04d53b0ebf09f6
MarikSmerch <arambo202@gmail.com>
2025-08-06 02:07:08 +0300

    fix 1.7

--- routes/webapp.py
+++ routes/webapp.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Request, HTTPException
 from bot.db import SessionLocal
 from bot.db_requests import get_or_create_user_by_telegram_init_data
 
-router = APIRouter(prefix="/api")
+router = APIRouter()
 
 
 @router.post("/init")
