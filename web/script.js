541ccad77bb52a3ac7f37ac654ccd82a29efc072
MarikSmerch <arambo202@gmail.com>
2025-08-06 00:06:09 +0300

    script 5.0

--- web/script.js
+++ web/script.js
@@ -4,14 +4,9 @@ window.addEventListener("DOMContentLoaded", () => {
   };
 
   log("🧠 Скрипт загружен");
-
-  if (window.Telegram && window.Telegram.WebApp) {
-    const tg = window.Telegram.WebApp;
-    tg.ready();
-    tg.expand(); 
-    const username = tg.initDataUnsafe?.user?.username || "гость";
-    log(`✅ WebApp активен: ${username}`);
-  } else {
-    log("❗ Telegram.WebApp не найден — ты открыл страницу вне Telegram");
-  }
+  const tg = window.Telegram.WebApp;
+  tg.ready();
+  tg.expand(); 
+  const username = tg.initDataUnsafe?.user?.username || "гость";
+  log(`✅ WebApp активен: ${username}`);
 });
