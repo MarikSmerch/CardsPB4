e518be3db3df3bce8ae4f5dfca4c0e6a94ba8e52
MarikSmerch <arambo202@gmail.com>
2025-08-06 04:50:05 +0300

    fix 1.0

--- bot/bot.py
+++ bot/bot.py
@@ -4,14 +4,13 @@ from db import create_all_tables
 from dotenv import load_dotenv
 import os
 
-from handlers import start
-
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 
 
 if __name__ == '__main__':
+    from handlers import start
     create_all_tables()
 
     app = ApplicationBuilder().token(TOKEN).build()
