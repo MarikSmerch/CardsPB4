d2d9a105e33d4479eae0e545b56f21f75bb56a29
MarikSmerch <arambo202@gmail.com>
2025-08-21 14:09:21 +0300

    fix 1.1

--- appendDB/reassign_prizes.py
+++ appendDB/reassign_prizes.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if BASE_DIR not in sys.path:
     sys.path.insert(0, BASE_DIR)
 
-from bot.db.base import SessionLocal, engine
+from bot.db import SessionLocal, engine
 from bot.db.models import Base, Card
 from prizes_seed import seed_prizes
 
